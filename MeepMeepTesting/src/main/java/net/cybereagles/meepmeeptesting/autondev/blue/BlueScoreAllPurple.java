2ce4239cab06cbd939b1fbb4543b6edab74646f3
SkyfireDragon22 <lingianna22@gmail.com>
2025-11-04 18:05:53 -0600

    Addition to Auton

--- MeepMeepTesting/src/main/java/net/cybereagles/meepmeeptesting/autondev/blue/BlueScoreAllPurple.java
+++ MeepMeepTesting/src/main/java/net/cybereagles/meepmeeptesting/autondev/blue/BlueScoreAllPurple.java
@@ -17,29 +17,64 @@ public class BlueScoreAllPurple {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setDimensions(18,18)
                 .build();
 
 //        Actions:
 
-        Pose2d startingPos = new Pose2d(-9,-9,deg(270));
+        Pose2d startingPos = new Pose2d(-26.5,-9,deg(270));
+        Pose2d existingPos = startingPos;
 
-        Action moveToScoreLocation = myBot.getDrive().actionBuilder(startingPos)
+        Action collectFirstBall = myBot.getDrive().actionBuilder(startingPos)
+                .splineToConstantHeading(new Vector2d(-11.5, -40), deg(270))
+                .build();
+
+        Action moveToScoreLocation = myBot.getDrive().actionBuilder(new Pose2d(-11.5,-40,deg(270)))
                 .setTangent(deg(180))
                 .splineToLinearHeading(new Pose2d(-40, -40, deg(220)), deg(270))
                 .build();
 
-        Action moveAwayFromScoreLocation = myBot.getDrive().actionBuilder(new Pose2d(-40, -40, deg(220)))
+        Action moveAwayFromScoreLocation = myBot.getDrive().actionBuilder(new Pose2d(-40,-40,deg(220)))
                 .strafeTo(new Vector2d(-40,0))
+                .turnTo(0)
                 .build();
 
+        Action collectSecondBall = myBot.getDrive().actionBuilder(new Pose2d(-40,0,deg(0)))
+                .splineToLinearHeading(new Pose2d(11.8, -40, deg(270)), deg(270))
+                .build();
+        Action moveToScoreLocation2 = myBot.getDrive().actionBuilder(new Pose2d(11.8, -40, deg(270)))
+                .setTangent(deg(90))
+                .splineToLinearHeading(new Pose2d(-40,-40,deg(220)),deg(270))
+                .build();
+
+        Action collectThirdBall = myBot.getDrive().actionBuilder(new Pose2d(-40,-40,deg(220)))
+                .splineTo(new Vector2d(35,-40),deg(270))
+                .build();
+
+
         Action fullShoot = new SequentialAction(
+//                startIntake(),
+                collectFirstBall,
+                new SleepAction(2),
+//                stopIntake(),
 //                startLauncher(),
                 moveToScoreLocation,
 //                startSingleBallLoader(),
                 new SleepAction(4),
 //                stopSingleBallLoader(),
 //                stopLauncher(),
-                moveAwayFromScoreLocation
+                moveAwayFromScoreLocation,
+//                startIntake(),
+                collectSecondBall,
+                new SleepAction(2),
+//                stopIntake(),
+//                startLauncher(),
+                moveToScoreLocation2,
+                new SleepAction(4),
+//                stopLauncher(),
+//                startIntake(),
+                collectThirdBall,
+                new SleepAction(2)
         );
 
         myBot.runAction(fullShoot);
