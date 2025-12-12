453bc8c810d898bd03be75f323f8e84ad45d1455
chris <chris.tenscok@gmail.com>
2025-12-12 11:34:32 -0600

    autons for thirdbot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/thirdbot/RobotTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/thirdbot/RobotTest.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.auton.thirdbot;
 
+import static org.firstinspires.ftc.teamcode.auton.Util.deg;
+
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
@@ -8,8 +10,10 @@ import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.auton.thirdbot.third_bot_modules.intake.Intake;
 import org.firstinspires.ftc.teamcode.auton.thirdbot.third_bot_modules.launcher.PIDFlyWheel;
 import org.firstinspires.ftc.teamcode.auton.thirdbot.third_bot_modules.loader.TripleBallQuadLoader;
 
@@ -17,11 +21,19 @@ import org.firstinspires.ftc.teamcode.auton.thirdbot.third_bot_modules.loader.Tr
 public class RobotTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        Pose2d startingPos = new Pose2d(0,0,deg(0));
+        //MecanumDrive drive = new MecanumDrive(hardwareMap,startingPos);
         PIDFlyWheel launcher = new PIDFlyWheel(hardwareMap, telemetry);
         TripleBallQuadLoader loader = new TripleBallQuadLoader(hardwareMap);
-
-        Action runLauncher = new ParallelAction(
-                launcher.revLauncher(250)
+        Intake intake = new Intake(hardwareMap, telemetry);
+//        Action runLauncher = new ParallelAction(
+//                launcher.revLauncher(250)
+//        );
+        Action runIntake = new ParallelAction(
+                intake.startActiveIntake()
+        );
+        Action stopIntake = new ParallelAction(
+                intake.stopActiveIntake()
         );
 
         Action runLoader = new ParallelAction(
@@ -30,10 +42,11 @@ public class RobotTest extends LinearOpMode {
 
         waitForStart();
 
-        Actions.runBlocking(new ParallelAction(
+        Actions.runBlocking(new SequentialAction(
+                runIntake,
                 runLoader,
-                new SleepAction(2),
-                runLauncher
+                new SleepAction(30),
+                stopIntake
         ));
 
     }
