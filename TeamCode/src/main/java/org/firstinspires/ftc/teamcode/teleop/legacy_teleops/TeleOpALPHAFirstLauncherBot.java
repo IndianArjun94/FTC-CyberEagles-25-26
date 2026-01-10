105f626cddee398298ffcd9e2b826a460d2d04ef
SkyfireDragon22 <lingianna22@gmail.com>
2026-01-09 19:51:55 -0600

    Old Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/legacy_teleops/TeleOpALPHAFirstLauncherBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/legacy_teleops/TeleOpALPHAFirstLauncherBot.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.teleop;
+package org.firstinspires.ftc.teamcode.teleop.legacy_teleops;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
